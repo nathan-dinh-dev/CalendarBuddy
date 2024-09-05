666ab086fb2fbbb5269477fd7d651401e82bb6ce
Nathan Dinh <nathanxdinhx97@gmail.com>
2024-09-04 17:42:00 -0700

    feat: initial screen shows current month and day

---
+++ CS151/src/calendar/DateTimeExample.java
@@ -0,0 +1,53 @@
+package calendar;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class DateTimeExample {
+	public static void main(String[] args) {
+		LocalDate cal = LocalDate.now();
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Today: ");
+		printCalendar(cal);
+
+		while (sc.hasNextLine()) {
+			String input = sc.nextLine();
+			if (input.equals("p")) {
+				cal = cal.minusMonths(1); // LocalDateTime is immutable
+				printCalendar(cal);
+			} else if (input.equals("n")) {
+				cal = cal.plusMonths(1); // LocalDateTime is immutable
+				printCalendar(cal);
+
+			}
+		}
+		System.out.println("Bye!");
+	}
+
+	public static void printCalendar(LocalDate c) {
+		System.out.print(c.getDayOfWeek());
+		System.out.print(" ");
+		System.out.print(c.getDayOfMonth());
+		System.out.print(" ");
+		System.out.println(c.getMonth());
+
+		// To print a calendar in a specified format.
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("E, MMM d yyyy");
+		System.out.println(" " + formatter.format(c));
+
+		// To figure out the day of week of the 1st day of the given month
+		LocalDate x = LocalDate.of(c.getYear(), c.getMonth(), 1);
+		System.out.println(x.getDayOfWeek() + " is the day of " + c.getMonth() + " 1."); // enum value as it is
+		System.out.println(
+				x.getDayOfWeek().getValue() + " is an integer value corresponding " + " to " + x.getDayOfWeek()); // int
+																													// value
+																													// corresponding
+																													// to
+																													// the
+																													// enum
+																													// value
+
+	}
+
+}
--- CS151/src/calendar/MyCalendarTester.java
+++ CS151/src/calendar/MyCalendarTester.java
@@ -1,10 +1,43 @@
 package calendar;
 
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
 public class MyCalendarTester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		LocalDate today = LocalDate.now();
+		displayMonthCalendar(today);
 	}
 
+	public static void displayMonthCalendar(LocalDate today) {
+		LocalDate date = LocalDate.of(today.getYear(), today.getMonthValue(), 1);
+		System.out.println(date.getMonth().getDisplayName(TextStyle.FULL, Locale.US) + " " + date.getYear());
+		System.out.println("Su Mo Tu We Th Fr Sa");
+
+		int startDay = date.getDayOfWeek().getValue();
+		int daysInMonth = date.lengthOfMonth();
+
+		// Adjust start day index to match Java's start (1 = Monday) to our needs (0 =
+		// Sunday)
+		startDay = (startDay == 7) ? 0 : startDay;
+
+		for (int i = 0; i < startDay; i++) {
+			System.out.print("   ");
+		}
+
+		for (int day = 1; day <= daysInMonth; day++) {
+			String dayString = String.format("%2d", day);
+			if (day == today.getDayOfMonth()) {
+				dayString = "[" + dayString + "]";
+			}
+			System.out.print(dayString + " ");
+
+			if ((day + startDay) % 7 == 0) {
+				System.out.println();
+			}
+		}
+		System.out.println();
+	}
 }
